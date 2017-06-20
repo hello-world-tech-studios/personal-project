ae602cf6839c235aa64a2d050ddf2f9ee57a6ed6
Hack Reactor Student <student@hackreactor.com>
2017-06-20 16:32:55 -0500

    work on

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
   /* Your Javascript Here */
-
+  $('.the-button').on('click', function() {
+    var  e = prompt("what company would you like to learn about?")
+    if (e === "google"){
+      alert("copy and paste this url: https://www.google.com")
+    }else if(e === "stack overflow"){
+      alert("copy and paste this url: https://www.stackoverflow.com")
+    }else if (e === "amazon"){
+      alert("copy and paste this url: https://www.amazon.com")
+    }
+  });
 });
