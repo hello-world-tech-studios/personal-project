b9a65bda1661090a0b2109984f8cfe93b1e25706
Hack Reactor Student <student@hackreactor.com>
2017-06-21 16:30:14 -0500

    worked on button and links

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
   /* Your Javascript Here */
+  $("button").on('click', function() {
+    $(".llamacorn") .attr("src", "https://i.giphy.com/media/5DlqgSpUFU4bS/giphy.webp")
+    setTimeout(function(){
+
+    $(".llamacorn") .attr("src", "https://pbs.twimg.com/profile_images/378800000250050183/508fcaf746ea9074647b86b8d05953f0_400x400.jpeg")
+
+    }, 5000)
+    // find a way to change the src attribute
+  });
 
 });
