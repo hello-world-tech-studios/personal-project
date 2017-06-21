d074b384708433b0b2cb4c9429610b0d4c12e995
Hack Reactor Student <student@hackreactor.com>
2017-06-21 16:32:12 -0500

    awesome, still needs work though

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ $(document).ready(function() {
 
   /* Your Javascript Here */
   $('.the-button').on('click', function() {
-    var  e = prompt("what company would you like to learn about?")
-    if (e === "google"){
-      alert("copy and paste this url: https://www.google.com")
-    }else if(e === "stack overflow"){
-      alert("copy and paste this url: https://www.stackoverflow.com")
-    }else if (e === "amazon"){
-      alert("copy and paste this url: https://www.amazon.com")
-    }
+    var  e = prompt("what company would you like to go to?")
+    location.href = 'http://www.' + e + '.com';
+  });
+  $('.thelink1').on('click', function() {
+    var  a = alert("you need to get a developer to write your code (the best developer: max vale) and come up with an idea and a name, then you buy the name, here is a website where you can buy the name.")
+
+  });
+  $('.sponsors').on('click', function() {
+    var b = alert("we currently have no sponsors, you could be the first and get free advertising.")
+  });
+  $('.home').on('click', function() {
+    var c = alert("you are at the home page.")
+    
   });
 });
