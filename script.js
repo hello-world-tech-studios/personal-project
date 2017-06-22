e9a3538af701cb2aba5abfae165e0d6c6fa35716
Hack Reactor Student <student@hackreactor.com>
2017-06-22 15:28:26 -0500

    Facebook Project

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
   /* Your Javascript Here */
+  $("#submit").click(function () {
+var myStatus=$("#status").val()
+if (myStatus === "") {
+  alert("You need to enter a status")
+}else{
+  $(".post").append("<div class = 'message'>" + "<img class = 'avatar' src = 'http://i.annihil.us/u/prod/marvel/i/mg/2/00/53710b14a320b.png'>"+ "<div class='text'>" + myStatus + "<button>Like</button></div></div>")
+  $()
+}
 
+})
 });
