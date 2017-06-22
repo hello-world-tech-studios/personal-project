3a253f8a3f8b7fb82758f4f7506811f80216f410
Hack Reactor Student <student@hackreactor.com>
2017-06-22 15:27:44 -0500

    message

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
 
   /* Your Javascript Here */
-  $('.the-button').on('click', function() {
-    var  e = prompt("what company would you like to go to?")
+  $('.theButton').on('click', function() {
+    var e = prompt("what company would you like to go to?");
     location.href = 'http://www.' + e + '.com';
   });
   $('.thelink1').on('click', function() {
-    var  a = alert("you need to get a developer to write your code (the best developer: max vale) and come up with an idea and a name, then you buy the name, here is a website where you can buy the name.")
+    var  a = alert("you need to get a developer to write your code (the best developer: max vale) and come up with an idea and a name, then you buy the name, here is a website where you can buy the name.");
 
   });
   $('.sponsors').on('click', function() {
-    var b = alert("we currently have no sponsors, you could be the first and get free advertising.")
+    var max = alert("we currently have no sponsors, you could be the first and get free advertising. Call (512)-343-5614");
   });
   $('.home').on('click', function() {
-    var c = alert("you are at the home page.")
+    var c = alert("you are at the home page.");
     
   });
 });
