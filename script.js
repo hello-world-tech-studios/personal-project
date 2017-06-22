19a442f4b8329cda2f4cb5c8bad552026de2da72
Hack Reactor Student <student@hackreactor.com>
2017-06-22 15:27:50 -0500

    made a unicorn message button

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 $(document).ready(function() {
 
   /* Your Javascript Here */
-  $("button").on('click', function() {
+  $("#pictures").on('click', function() {
     $(".llamacorn") .attr("src", "https://i.giphy.com/media/5DlqgSpUFU4bS/giphy.webp")
+    setTimeout(function(){
+
+    $(".llamacorn") .attr("src", "https://s-media-cache-ak0.pinimg.com/originals/3b/64/68/3b6468f1bcc9ba22ecef1742ba6ed17d.jpg")
+    setTimeout(function(){
+
+    $(".llamacorn") .attr("src", "https://s-media-cache-ak0.pinimg.com/736x/13/ea/30/13ea30a7313f84265797ce8d2b770672.jpg")  
     setTimeout(function(){
 
     $(".llamacorn") .attr("src", "https://pbs.twimg.com/profile_images/378800000250050183/508fcaf746ea9074647b86b8d05953f0_400x400.jpeg")
 
     }, 5000)
-    // find a way to change the src attribute
+
+    },5000)
+
+    }, 5000)
+
+// find a way to change the src attribute
   });
 
+$("#message").on("click", function(){
+prompt("send a message to a real unicorn!") var unicornMessage=Math.floor(Math.random() * 4)
+if(unicornMessage=0){
+alert("Hello, friend I hope you are having a magical day! I hope that unicorns fart rainbows all over your day🦄🦄🦄🌈🌈🌈🌈!")}
+else if(unicornMessage=1){
+alert("Hello friend, thank you for the wonderful message you sent me, I hope you have a macical day!!")}
+else if(unicornMessage=2){
+alert("Hello friend, thank you for taking time to write me that lovely letter! I hope you have a wonderful day full of rainbows and sunshine!")
+}
+else if(unicornMessage=3){}
+}
+})
+
 });
+
+
+// Math.floor(Math.random() * 4)
