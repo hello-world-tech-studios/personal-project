e31d32494cf1428b83f013d58b18454e13d6b635
Hack Reactor Student <student@hackreactor.com>
2017-06-23 15:45:12 -0500

    Facebook website

--- script.js
+++ script.js
@@ -6,9 +6,25 @@ var myStatus=$("#status").val()
 if (myStatus === "") {
   alert("You need to enter a status")
 }else{
-  $(".post").append("<div class = 'message'>" + "<img class = 'avatar' src = 'http://i.annihil.us/u/prod/marvel/i/mg/2/00/53710b14a320b.png'>"+ "<div class='text'>" + myStatus + "<button>Like</button></div></div>")
-  $()
+  $(".post").append("<div class = 'message'>" + "<div><img class = 'avatar' src = 'http://i.annihil.us/u/prod/marvel/i/mg/2/00/53710b14a320b.png'><button class = 'likes'>Like</button></div>"+ "<div class='text'>" + myStatus + "</div></div>")
+  
 }
-
+  
 })
+$("body").on("click", ".likes", function() {
+  var color = $(this).css("background-color");
+  if (color === "rgb(192, 192, 192)") {
+    $(this).css("background-color", "lightblue");
+  } else {
+    $(this).css("background-color", "rgb(192, 192, 192)")
+  }
+  
+})
+$("body").on("keypress", function(event) {
+    if (event.key == "f") {
+    $("body").css("background-color", "black")
+  $("body").append("<a href = 'http://www.wearetopsecret.com/' id = 'secret' >You have found the secret page</a>")
+  $(".card").hide()
+}
+});
 });
